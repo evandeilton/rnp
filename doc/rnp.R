0864cd4093ffa1a548e1ff0bf25b6dd4447bb8fe
evandeilton <evandeilton@gmail.com>
2019-03-02 00:58:33 -0300

    Adiciona pasta /doc e atualiza dependencias

---
+++ doc/rnp.R
@@ -0,0 +1,13 @@
+## ---- eval=FALSE, echo=TRUE----------------------------------------------
+#    # Se não tiver o devtools, instalar.
+#    if(!require(devtools)){
+#      install.packages("devtools")
+#    }
+#  
+#    # Instalar o rnp
+#    devtools::install_github("evandeilton/rnp")
+#  
+#    # Carregar o pacote
+#    require("rnp")
+#    ?rnp::rnp_freq
+
